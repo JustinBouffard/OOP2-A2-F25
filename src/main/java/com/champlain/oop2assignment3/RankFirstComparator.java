9ae002b805167b9d2aa35243f13237f33cd23e64
JustinBouffard <bouffardjustin@gmail.com>
2025-10-05 16:40:07 -0400

    Added JavaDoc comments for the RankFirstComparator feature.

--- src/main/java/com/champlain/oop2assignment3/RankFirstComparator.java
+++ src/main/java/com/champlain/oop2assignment3/RankFirstComparator.java
@@ -3,7 +3,20 @@ package com.champlain.oop2assignment3;
 
 import java.util.Comparator;
 
+/**
+ * A {@link Comparator} used to sort {@link Card} objects based on a rank first algorithm.
+ *
+ * @author Justin Bouffard
+ */
 public class RankFirstComparator implements Comparator<Card> {
+    /**
+     * Compares two {@link Card} objects by rank first, then by suit if the ranks are equal.
+     *
+     * @param card1 The first card to compare.
+     * @param card2 The second card to compare.
+     * @return Negative int if {@code card1} is less than {@code card2} and a positive int if {@code card1} is greater than {@code card2}
+     * based on rank, then based on suit if they are the same rank.
+     */
     @Override
     public int compare(Card card1, Card card2) {
         int rankComparison = card1.getRank().compareTo(card2.getRank());
